3e8f12571b9c96e10aca5406a3ea17ecee0f7dbb
abhisheknair10 <nairabs10@gmail.com>
2022-11-30 00:07:19 -0500

    rpi

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -142,8 +142,6 @@ while True:
             #Print Latitude and Longtitude Values
             y += font20.getsize(metricsmsg)[1]
             draw.text((x, y + 5), f"Coord: {str(lat)}, {str(lng)}", font=font16, fill="#FFFFFF")
-            y += font16.getsize(strLat)[1]
-            draw.text((x, y + 10), strLong, font=font16, fill="#FFFFFF")
 
             # Display Distance.
             coords_1 = (startLat, startLon)
@@ -153,11 +151,11 @@ while True:
             totDist += distance
 
             y += font16.getsize(strLong)[1]
-            draw.text((x, y + 15), "Distance: " + str(totDist) + " miles", font=font16, fill="#FFFFFF")
+            draw.text((x, y + 10), "Distance: " + str(totDist) + " miles", font=font16, fill="#FFFFFF")
 
             # Display Time
             y += font16.getsize("Distance: " + str(totDist))[1]
-            draw.text((x, y + 20), "Time: "+str(totTime), font=font16, fill="#FFFFFF")
+            draw.text((x, y + 15), "Time: "+str(totTime), font=font16, fill="#FFFFFF")
 
             draw.text((x, height - 30), "<--- End Run", font=font20, fill="#FF4949")
 
